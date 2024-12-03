26042fd328fdb84415814ebb1813e457163b8b22
Grace Fujinaga <gracefujinaga@gmail.com>
2024-12-03 11:39:59 -0600

    cleaned simulation, added metrics

--- Simulation.py
+++ Simulation.py
@@ -23,11 +23,15 @@ from functools import partial, wraps
 import simpy # discrete event simulation environment
 import random
 
+import matplotlib.pyplot as plt
+
 '''
-Problem Set Up
+Problem Set Up, Global Vars
 '''
+# problem set up 
 SIM_TIME = 100
 CALL_ARRIVAL_RATE = 2
+
 
 #Metrics
 total_wait_time = 0
@@ -36,12 +40,12 @@ total_calls = 0
 total_reneged_calls = 0
 total_escalated_calls = 0
 service_times = []
-wait_times = []  #Store wait times for visualization
-escalated_wait_times =[]
-call_states = {"Connected": 0, "Reneged": 0}  #Track call outcomes
+wait_times = []  
+escalated_service_times = []
+escalated_wait_times = []
 
 ## set up for queue 1
-num_sp = 3  # number of call center service providers
+num_sp = 6  # number of call center service providers
 min_service_time_1 = 2 # minutes (60 seconds)
 mean_service_time_1 = 5 # minutes (120 seconds)
 max_service_time_1 = 25 # minutes (300 seconds)
@@ -64,53 +68,22 @@ fixed_simulation_time =  simulation_hours*60*60
 parameter_string_list = [str(simulation_hours),'hours', str(num_sp), str(num_managers)]
 separator = '-'        
 simulation_file_identifier = separator.join(parameter_string_list)
+simulation_trace_file_name = '/Users/gracefujinaga/Documents/Northwestern/MSDS_460/msds460_final_project/results/simulation-program-trace-' + simulation_file_identifier + '.txt'
+event_log_file_name = '/Users/gracefujinaga/Documents/Northwestern/MSDS_460/msds460_final_project/results/simulation-event-log-' + simulation_file_identifier + '.csv'
 
 '''
 Set up helper functions for queue 1
 '''
-    
-# def call_process(env, csr, call_id):
-#     global total_wait_time, total_calls, total_reneged_calls, total_escalated_calls
-    
-#     arrival_time = env.now
-#     print(f"Call {call_id} arrived at {arrival_time:.2f}")
-
-#     #Request CSR
-#     with csr.request() as req:
-#         #Check if CSR is available or call will wait in the hold queue
-#         wait_result = yield req | env.timeout(random.uniform(*HOLD_DURATION))
-#         if req in wait_result:
-#             #CSR became available
-#             wait_time = env.now - arrival_time
-#             total_wait_time += wait_time
-#             wait_times.append(wait_time)
-#             call_states["Connected"] += 1
-#             print(f"Call {call_id} connected to CSR after waiting {wait_time:.2f} minutes")
-            
-#             #Call duration while CSR is engaged
-#             yield env.timeout(random.uniform(*CALL_DURATION))
-#             print(f"Call {call_id} completed at {env.now:.2f}")
-#         else:
-#             #Call dropped if CSR not available within hold time
-#             total_dropped_calls += 1
-#             call_states["Dropped"] += 1
-#             wait_times.append(HOLD_DURATION[1])  #Max hold time as wait time for dropped calls
-#             print(f"Call {call_id} dropped after waiting on hold")
-
-#     total_calls += 1
-
-
 def call_process_1(env, caseid_queue, event_log):
-    global total_wait_time, total_calls, total_service_time
+    global total_wait_time, total_calls, total_service_time, service_times, wait_times
 
     # track total arrival time
     arrival_time = env.now
 
     # adding renege
     curr_queue_length = len(caseid_queue)
-    print(f"curr queue length: {curr_queue_length}")
     if (curr_queue_length > reneg_queue_length_1):
-        renege(0, caseid_queue)
+        renege(1, caseid_queue)
         return
 
     # must have available sp to provide service... freeze until request can be met
@@ -122,10 +95,10 @@ def call_process_1(env, caseid_queue, event_log):
 
         wait_time = env.now - arrival_time
         total_wait_time += wait_time
-        wait_times.append(wait_time)
-        call_states["Connected"] += 1
-        print(f"Call {caseid} connected to sp after waiting {wait_time:.2f} minutes")
+        wait_times.append(wait_time / 60)
 
+        # call_states["Connected"] += 1
+        begin_service = env.now 
         print("Begin_service caseid =",caseid,'time = ',env.now,'initial_queue_length =',queue_length_on_entering_service)
         env.process(event_log_append(env, caseid, env.now, 'begin_service', event_log)) 
 
@@ -135,38 +108,59 @@ def call_process_1(env, caseid_queue, event_log):
         yield env.timeout(service_time)  
 
         # update metrics
-        service_time = env.now - wait_time
+        service_time = env.now - begin_service
         total_service_time += service_time
-        service_times.append(service_time)
+        service_times.append(service_time / 60)
 
-        queue_length_on_leaving_service = len(caseid_queue)
-        print("End_service caseid =",caseid,'time = ',env.now,'initial_queue_length =',queue_length_on_leaving_service)
+        queue_len_on_leaving_service = len(caseid_queue)
+        print("End_service caseid =",caseid,'time = ',env.now,'curr_queue_length =',queue_len_on_leaving_service)
         env.process(event_log_append(env, caseid, env.now, 'end_service', event_log))
 
 '''
 Helpers for part 2 of the queue
 '''
 def call_process_2(env, caseid_queue, event_log):
-    global total_wait_time, total_calls, total_reneged_calls, total_escalated_calls, total_service_time
-    # must have available manager to provide service
+    global total_wait_time, total_calls, total_service_time, escalated_service_times, escalated_wait_times
+
+    # track total arrival time
+    arrival_time = env.now
+
+    # adding renege
+    curr_queue_length = len(caseid_queue)
+    if (curr_queue_length > reneg_queue_length_2):
+        renege(2, caseid_queue)
+        return
+
+    # must have available sp to provide service... freeze until request can be met
     with available_managers.request() as req_2:
-        yield req_2  # wait until the request can be met
+        yield req_2  
+
         queue_length_on_entering_service = len(caseid_queue)
         caseid = caseid_queue.popleft()
 
+        wait_time = env.now - arrival_time
+        total_wait_time += wait_time
+        escalated_wait_times.append(wait_time / 60)
+
+        # call_states["Connected"] += 1
         print("Begin_escalated_service caseid =",caseid,'time = ',env.now,'escalated_queue_length =',queue_length_on_entering_service)
-        env.process(event_log_append(env, caseid, env.now, 'begin_escalated_service', event_log)) 
+        begin_service = env.now 
+        env.process(event_log_append(env, caseid, env.now, 'begin_escalated_service', event_log))
 
-        # schedule end_service event based on service_time
+        # schedule end_service event based on normally distributed service_time
         service_time = round(60*random.uniform(min_service_time_2, max_service_time_2))  
-        yield env.timeout(service_time)  # sets begin_service as generator function
+        yield env.timeout(service_time)  
 
-        queue_length_on_leaving_service = len(caseid_queue)
+        # update metrics
+        service_time = env.now - begin_service
+        total_service_time += service_time
+        escalated_service_times.append(service_time/ 60)
 
-        print("End_escalated_service caseid =",caseid,'time = ',env.now,'escalated_queue_length =',queue_length_on_leaving_service)
+        queue_len_on_leaving_service = len(caseid_queue)
+        print("End_escalated_service caseid =",caseid,'time = ',env.now,'curr_queue_length =',queue_len_on_leaving_service)
         env.process(event_log_append(env, caseid, env.now, 'end_escalated_service', event_log))
-    
 
+# if id is 2, then it is queue 2, if it is 1, queue 1
 def renege(id, queue) :
     global total_reneged_calls
     total_reneged_calls += 1
@@ -178,9 +172,9 @@ def renege(id, queue) :
     else : 
         print_string = "Customer renege caseid="
 
-    print("reneged process 1 =",caseid,'time = ',env.now,'initial_queue_length =',len(queue) + 1)
     print(print_string,caseid,'time = ',env.now,'queue_length =',len(queue) )
-    env.process(event_log_append(env, caseid, env.now, 'renege', event_log)) 
+    activity = f"renege_queue_{id}"
+    env.process(event_log_append(env, caseid, env.now, activity, event_log)) 
 
 '''
 Starting function for the simulation
@@ -195,8 +189,9 @@ def initial_call(env, caseid, caseid_queue_1, caseid_queue_2, event_log):
     while True:  
         inter_arrival_time = round(60*np.random.exponential(scale = CALL_ARRIVAL_RATE))
         print("NEXT ARRIVAL TIME: ", env.now + inter_arrival_time)
-
         yield env.timeout(inter_arrival_time)  # generator function waits
+
+        total_calls += 1
 
         caseid += 1
         arrival_time = env.now
@@ -208,22 +203,20 @@ def initial_call(env, caseid, caseid_queue_1, caseid_queue_2, event_log):
 
         yield env.timeout(0) 
 
-        # TODO: we would change right here
-        if len(caseid_queue_1) > 0:
-            print("Customer joins queue caseid =",caseid,'time = ',env.now,'initial_queue_length =',len(caseid_queue_1))
-            time = env.now
+        #if len(caseid_queue_1) > 0:
+        print("Customer joins queue caseid =",caseid,'time = ',env.now,'initial_queue_length =',len(caseid_queue_1))
+        time = env.now
              
-            activity = 'join_queue'
-            env.process(event_log_append(env, caseid, time, activity, event_log)) 
-            env.process(call_process_1(env,  caseid_queue_1, event_log))   
+        activity = 'join_queue_1'
+        env.process(event_log_append(env, caseid, time, activity, event_log)) 
+        env.process(call_process_1(env,  caseid_queue_1, event_log))   
 
-            if random.random() < 0.15:
-                total_escalated_calls += 1
-                caseid_queue_2.append(caseid)   
-                activity = 'service request escalated'
-                if len(caseid_queue_2) < reneg_queue_length_2:
-                    env.process(event_log_append(env, caseid, time, activity, event_log)) 
-                    env.process(call_process_2(env,  caseid_queue_2, event_log))
+        if random.random() < 0.15:
+            total_escalated_calls += 1
+            caseid_queue_2.append(caseid) 
+            activity = 'join_queue_2 - service request escalated'
+            env.process(event_log_append(env, caseid, time, activity, event_log)) 
+            env.process(call_process_2(env,  caseid_queue_2, event_log))
 
 '''        
 ---------------------------------------------------------
@@ -286,16 +279,10 @@ if obtain_reproducible_results:
 
 # set up simulation trace monitoring for the simulation
 data = []
-
-# bind *data* as first argument to monitor()
-this_trace_monitor = partial(trace_monitor, data)
-
+curr_trace_monitor = partial(trace_monitor, data)
 env = simpy.Environment()
-trace(env, this_trace_monitor)
-
+trace(env, curr_trace_monitor)
 env.process(test_process(env))
-
-# implement FIFO queue to hold caseid values
 
 ## NOTE THAT THESE are DEQUES: 
 # append function puts caseid at the RIGHT of the queue
@@ -304,24 +291,23 @@ env.process(test_process(env))
 caseid_queue_1 = deque()
 caseid_queue_2 = deque()
 
-# set up limited sp resource
+# set up limited sp, manager resource
 available_sp = simpy.Resource(env, capacity = num_sp)
 available_managers = simpy.Resource(env, capacity = num_managers)
 
-# dummy caseid values - will be omitted from the event_log
-# prior to analyzing simulation results
+# dummy caseid value to start the simulation
 caseid = -1  
 
 # beginning record in event_log list of tuples of the form
 # form of the event_log tuple item: (caseid, time, activity)
 event_log = [(caseid,0,'null_start_simulation')]
 
+# log events start the simulation
 env.process(event_log_append(env, caseid, env.now, 'start_simulation', event_log))
- 
-# call customer arrival process/generator to begin the simulation
 env.process(initial_call(env, caseid, caseid_queue_1, caseid_queue_2, event_log))  
 
-env.run(until = fixed_simulation_time)  # start simulation with monitoring and fixed end-time
+ # start simulation with monitoring and fixed end-time
+env.run(until = fixed_simulation_time) 
 
 '''
 -----------------------------------------------------------------
@@ -339,12 +325,12 @@ pandas data frame and saved to a comma-delimited text file
 
 -----------------------------------------------------------------
 '''
-simulation_trace_file_name = '/Users/gracefujinaga/Documents/Northwestern/MSDS_460/msds460_final_project/results/simulation-program-trace-' + simulation_file_identifier + '.txt'
+
+# write trace to trace file
 with open(simulation_trace_file_name, 'wt') as ftrace:
     for d in data:
-        print(str(d), file = ftrace)
-print()        
-print('simulation program trace written to file:',simulation_trace_file_name)
+        print(str(d), file = ftrace)       
+print('\n trace written to file:',simulation_trace_file_name)
 
 # convert list of tuples to list of lists
 event_log_list = [list(element) for element in event_log]
@@ -358,14 +344,64 @@ for d in event_log_list:
         caseid_list.append(d[0])
         time_list.append(d[1])
         activity_list.append(d[2])
-event_log_df = pd.DataFrame({'caseid':caseid_list,
-                             'time':time_list,
-                             'activity':activity_list})    
-print()
-print('event log stored as Pandas data frame: event_log_df')
 
-# save event log to comma-delimited text file
-event_log_file_name = '/Users/gracefujinaga/Documents/Northwestern/MSDS_460/msds460_final_project/results/simulation-event-log-' + simulation_file_identifier + '.csv'
+# convert to df and save to csv
+event_log_df = pd.DataFrame({'caseid':caseid_list, 'time':time_list,'activity': activity_list})    
 event_log_df.to_csv(event_log_file_name, index = False)  
-print()
-print('event log written to file:',event_log_file_name)
+print('\n event log written to file:',event_log_file_name)
+
+
+#Results
+average_wait_time = total_wait_time / (total_calls * 60) if total_calls > 0 else 0
+renege_rate = total_reneged_calls / (total_calls * 60) if total_calls > 0 else 0
+
+print("\nSimulation Results:")
+print(f"Total Calls: {total_calls}")
+print(f"Average Wait Time: {average_wait_time:.2f} minutes")
+print(f"Renege Rate: {renege_rate:.2%}")
+print(f"Total Reneged Calls: {total_reneged_calls}")
+print(f"Total Escalated Calls: {total_escalated_calls}")
+
+#Visualization
+plt.figure(figsize=(10, 6))
+plt.hist(wait_times, color='skyblue', edgecolor='black', alpha=0.7)
+plt.title("Distribution of Initial Wait Times")
+plt.xlabel("Wait Time (minutes)")
+plt.ylabel("Frequency")
+plt.show()
+
+# Service Time Distribution
+plt.figure(figsize=(10, 6))
+plt.hist(service_times, color='green', edgecolor='black', alpha=0.7)
+plt.title("Distribution of Initial Service Times")
+plt.xlabel("Service Time (minutes)")
+plt.ylabel("Frequency")
+plt.show()
+
+#Wait Time Distribution
+plt.figure(figsize=(10, 6))
+plt.hist(escalated_wait_times, color='skyblue', edgecolor='black', alpha=0.7)
+plt.title("Distribution of Escalated Wait Times")
+plt.xlabel("Wait Time (minutes)")
+plt.ylabel("Frequency")
+plt.show()
+
+# Service Time Distribution
+plt.figure(figsize=(10, 6))
+plt.hist(escalated_service_times, color='green', edgecolor='black', alpha=0.7)
+plt.title("Distribution of Escalated Service Times")
+plt.xlabel("Service Time (minutes)")
+plt.ylabel("Frequency")
+plt.show()
+
+
+# call statistics
+#Call Outcomes
+labels = ['resolved', 'reneged']
+print(total_calls - total_reneged_calls)
+sizes = [total_calls - total_reneged_calls, total_reneged_calls]
+
+plt.figure(figsize=(8, 8))
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140, colors=['lightgreen', 'lightcoral'])
+plt.title("Call Outcomes")
+plt.show()
